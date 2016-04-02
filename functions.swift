5f10a5f6434e5893695ea9ad20039fe620164be5
Michael P. Nitowski <mpnitowski@gmail.com>
2016-04-02 14:29:25 -0400

    Better directory function

--- functions.swift
+++ functions.swift
@@ -65,29 +65,44 @@ struct Directory : SequenceType
 
 struct DirectoryEntry
 {
-    enum FileType { case File, Directory, Unknown }
+    enum FileType { case Regular, Directory, Unknown }
     var name : String = ""
     var type : FileType = FileType.Unknown
+    func isDot() -> Bool {
+        return name == "." || name == ".."
+    }
 }
 
-func listDirectory(path: String) -> [String]? {
-    var directory = [String]()
+
+
+func listDirectory(path: String) -> [DirectoryEntry]? {
+    var directory = [DirectoryEntry]()
     let dp = opendir(path)
     if (dp == nil) {
         return nil
     }
     while(true) {
-       let ep = readdir(dp)
-       if (ep == nil) { break; }
-       let file = withUnsafePointer(&ep.memory.d_name) {
-           String.fromCString(UnsafePointer($0)) 
-       }
-       print(file)
-       if let fileName = file {
-            directory.append(fileName)
-       }
+        let ep = readdir(dp)
+        if (ep == nil) { break; }
+        let file = withUnsafePointer(&ep.memory.d_name) {
+            String.fromCString(UnsafePointer($0)) 
+        }
+        let dtype = ep.memory.d_type
+        print(dtype)
+        print(file)
+        if let fileName = file {
+            var entry = DirectoryEntry()
+            entry.name = fileName
+            var type = DirectoryEntry.FileType.Unknown
+            if Int(dtype) == Int(DT_REG) {
+                type = DirectoryEntry.FileType.Regular
+            } else if Int(dtype) == Int(DT_DIR) {
+                type = DirectoryEntry.FileType.Directory
+            }
+            entry.type = type
+            directory.append(entry)
+        }
     }
     closedir(dp)
     return directory
 }
-
